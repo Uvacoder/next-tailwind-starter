3015b3aa12fca1712104864b5dfa9b9e56484bf7
Uvacoder <88749643+Uvacoder@users.noreply.github.com>
2021-12-12 00:17:37 -0500

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,44 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const defaultTheme = require('tailwindcss/defaultTheme')
+const defaultColor = require('tailwindcss/colors')
+
+module.exports = {
+  darkMode: 'class',
+  content: ['./src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Plus Jakarta Sans', 'Inter UI', ...defaultTheme.fontFamily.sans],
+        mono: ['JetBrains Mono', ...defaultTheme.fontFamily.mono]
+      },
+      screens: {
+        standalone: { raw: '(display-mode: standalone)' }
+      },
+      colors: {
+        gray: defaultColor.neutral,
+        primary: defaultColor.blue,
+        secondary: defaultColor.indigo
+      },
+      typography: (theme) => ({
+        default: {
+          css: {
+            color: theme('colors.gray.700'),
+            a: {
+              color: theme('colors.primary.500'),
+              '&:hover': {
+                color: theme('colors.primary.600')
+              }
+            }
+          }
+        }
+      })
+    }
+  },
+  plugins: [
+    // Additional first-party plugins
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/aspect-ratio'),
+    require('@tailwindcss/line-clamp')
+  ]
+}
